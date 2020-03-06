220086c06dab6213504e69c81267b239f398b01f
Cristobal Honorato <chonorato@hcsmb020800209.hc.lan>
2020-03-06 16:03:36 +0100

    added tree image into the folder

--- src/LotrHeader.js
+++ src/LotrHeader.js
@@ -1,9 +1,10 @@
 import React from "react";
+import GondorTree from "./images/t-gondor-tree.png"
 
 const LotrHeader = props => {
   return (
     <div id="header">
-      <img src="https://png2.cleanpng.com/sh/e08824d6684ac1217368350847cacc6b/L0KzQYm3VMA0N6J2j5H0aYP2gLBuTgRpbV5xhARtLXBpPcXvhb1zcZ9si59qcofofn77kvVmapZmitY2d3jshLa0lQJmbV50Rd54cnSwf7e0lPhmNaNuhtl8LUXkc4ToVsIzbWo5etc6LkW7RIa5V8M2OWY3SqkCMUS8QIq8VcYveJ9s/kisspng-the-lord-of-the-rings-arwen-treebeard-white-tree-o-lord-of-the-rings-5ac3a622e94be1.5845273515227714909556.png" alt="gondor tree" id="gondor-tree" />
+      <img src={GondorTree} id="gondor-tree" />
       <h1
         style={{
           fontFamily: "Cinzel Decorative",
